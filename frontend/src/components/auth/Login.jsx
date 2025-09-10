1a27e6f6879a3edfb96bb2452252558a064b1fec
Limits-AWB <ianangelo.simbulan@achievewithoutborders.com>
2025-09-10 23:06:55 +0800

    REMOVED localhost

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -5,7 +5,7 @@ import Swal from "sweetalert2";
 import { useAuth } from "../../contexts/AuthContext";
 
 // API URL
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const API_URL = import.meta.env.VITE_API_URL;
 
 const Login = () => {
   const navigate = useNavigate();
--- frontend/src/components/auth/ResetPassword.jsx
+++ frontend/src/components/auth/ResetPassword.jsx
@@ -4,7 +4,7 @@ import { ClipLoader } from "react-spinners";
 import Swal from "sweetalert2";
 
 // API URL
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const API_URL = import.meta.env.VITE_API_URL;
 
 const ResetPassword = () => {
   const navigate = useNavigate();
